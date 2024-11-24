39c548926941e174db11980db079184f50cc06a1
Anton Kalesnikovich <ant.kolesnikovich@gmail.com>
2024-11-24 22:52:07 +0300

    Added books model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    is_created = models.DateTimeField(auto_now_add=True)
+    is_modified = models.DateTimeField(auto_now=True)
+    publication_date = models.DateField()
+    page_count = models.IntegerField()
+
+    def __str__(self):
+        return self.title
