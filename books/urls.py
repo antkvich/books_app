20de5099174252d9b931552f66b8ce627b99447d
Anton Kalesnikovich <ant.kolesnikovich@gmail.com>
2024-11-25 21:25:42 +0300

    Added ranked books biew

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookListCreateView
+from .views import BookListCreateView, RankedBooksView
 
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list'),
+    path('rankedBooks/', RankedBooksView.as_view(), name='ranked-book-list'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Window, F
+from django.db.models.functions import RowNumber
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -16,3 +18,23 @@ class BookListCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+
+class RankedBooksView(APIView):
+    def get(self, request):
+        start_date = request.query_params.get('start_date')
+        end_date = request.query_params.get('end_date')
+
+        if not start_date or not end_date:
+            return Response({"error": "Both start_date and end_date are required."}, status=400)
+
+        books = Book.objects.filter(
+            publication_date__range=[start_date, end_date]
+        ).annotate(
+            rank=Window(
+                expression=RowNumber(),
+                order_by=F('page_count').desc()
+            )
+        ).values('id', 'title', 'page_count', 'rank')
+
+        return Response(books)
